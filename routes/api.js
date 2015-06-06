41bf66be4a4c85271167b99009de1ddce063ff41
Preston Pham <prestonp08@gmail.com>
2015-06-06 14:35:35 -0500

    hookup store to response

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,18 @@
-var express = require('express');
-var router = express.Router();
-var scraper = require('../lib/scraper');
-var _ = require('lodash');
+var _           = require('lodash');
+var express     = require('express');
+var router      = express.Router();
+var scraper     = require('../lib/scraper');
+var store       = require('../lib/store');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   var fields = _.pick(req.query, 'Address1', 'City', 'ZipCode');
   fields.DistrictType = 'ALL';
   fields.Submit1 = 'Submit';
   fields.Address1 = fields.Address1.replace(/\s/g, '+');
 
-  scraper.query(fields, function(err, response) {
+  scraper.query(fields, function(err, legislators) {
     if (err) return res.send(500);
-    // to do lookup key from response & send db values
-    return res.send(fields);
+    return res.send(legislators.map(store.get));
   });
 });
 
