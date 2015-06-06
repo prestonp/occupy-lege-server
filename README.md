3f7605a7d29633294d5f421d93281324a79f08f1
Preston Pham <prestonp08@gmail.com>
2015-06-06 13:45:25 -0500

    add scraping setup

--- README.md
+++ README.md
@@ -3,3 +3,17 @@ Occupy Lege Server
 
 Hack ATX!
 
+### Set up
+
+npm install
+
+### Run
+
+npm start
+
+### Test Locally
+
+```
+http://localhost:3000/api?ZipCode=78704&City=austin&Address1=3001+South+Congress+Avenue
+```
+
--- routes/api.js
+++ routes/api.js
@@ -5,12 +5,14 @@ var _ = require('lodash');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var fields = _.pick(req.query, 'address', 'city', 'zip');
+  var fields = _.pick(req.query, 'Address1', 'City', 'ZipCode');
+  fields.DistrictType = 'ALL';
+  fields.Submit1 = 'Submit';
+  fields.Address1 = fields.Address1.replace(/\s/g, '+');
 
   scraper.query(fields, function(err, response) {
     if (err) return res.send(500);
     // to do lookup key from response & send db values
-
     return res.send(fields);
   });
 });
